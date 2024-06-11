27e1c5898f213b6a3a6b23cf0a129dd2e17e113f
Alishkoo <baisholanov.alibek@gmail.com>
2024-06-11 23:12:38 +0500

    added cors

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,18 @@ import express from 'express';
 import connectDB from './db';
 import globalRouter from './global-router';
 import { logger } from './logger';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 connectDB();
 
+app.use(cors());
 app.use(logger);
 app.use(express.json());
 app.use('/api/v1/',globalRouter);
+
 
 
 app.get('/helloworld',(request,response) =>{
